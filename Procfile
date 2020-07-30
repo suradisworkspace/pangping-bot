799a12589bb59f333d8eb80d18f616664b0ee909
Suradis Sutampang <suradis.workspace@gmail.com>
2020-07-30 20:47:29 +0700

    run only web

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: node express.ts
-worker: node bot.ts
--- express.ts
+++ express.ts
@@ -1,5 +1,9 @@
 const path = require('path')
 const express = require('express')
+const { token } = require('./configs.json')
+const { CommandoClient } = require('discord.js-commando')
+const Keyv = require('keyv')
+const KeyvProvider = require('commando-provider-keyv')
 
 const app = express()
 app.use(express.static(path.join(__dirname, '/build')))
@@ -18,3 +22,37 @@ const port = process.env.PORT || 5000
 app.listen(port)
 
 console.log('App is listening on port ' + port)
+
+// DISCORD
+const queue = new Map()
+
+const client = new CommandoClient({
+  commandPrefix: '!',
+  unknownCommandResponse: false,
+  owner: '265037333915631616',
+  disableEveryone: true,
+  queue,
+})
+
+client.registry
+  .registerDefaultTypes()
+  .registerDefaultGroups()
+  .registerDefaultCommands({ help: false })
+  .registerGroups([
+    ['basic', 'Basic'],
+    ['music', 'Music Controller'],
+    ['meme', 'Meme Collection'],
+  ])
+  .registerCommandsIn(path.join(__dirname, 'commands'))
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+const settings = { serialize: (data) => data, deserialize: (data) => data }
+
+client.setProvider(
+  new KeyvProvider(new Keyv('mongodb://pangping:q1w2e3r4@ds147461.mlab.com:47461/heroku_cvn3w73l', settings))
+)
+
+client.login(token)
