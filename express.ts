ab0725e35cfbb38d6847300ed8c4e2046379429a
Suradis Sutampang <suradis.workspace@gmail.com>
2020-08-04 02:51:50 +0700

    init change settings with api

--- express.ts
+++ express.ts
@@ -6,38 +6,6 @@ const { body, validationResult } = require('express-validator')
 require('dotenv').config()
 const db = require('./src/helpers/db.ts')
 
-// const settings = { serialize: (data) => data, deserialize: (data) => data, namespace: 'users', collection: 'settings' }
-const app = express()
-app.use(express.static(path.join(__dirname, '/build')))
-
-app.get('/api/getList', (req, res) => {
-  var list = ['item1', 'item2', 'item3']
-  res.json(list)
-  console.log('Sent list of items')
-})
-
-app.get('/api/test', async (req, res) => {
-  var list = ['item1', 'item2', 'item3']
-  res.json(list)
-})
-
-app.get('/api/getServer', [body('token').not().isEmpty()], async (req, res) => {
-  const errors = validationResult(req)
-  console.log('errors', errors)
-  res.type('json')
-  var list = ['item1', 'item2', 'item3']
-  res.json(list)
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'))
-})
-
-const port = process.env.PORT || 5000
-app.listen(port)
-
-console.log('App is listening on port ' + port)
-
 // DISCORD
 const queue = new Map()
 
@@ -67,3 +35,41 @@ client.on('ready', () => {
 client.setProvider(new KeyvProvider(db))
 
 client.login(process.env.REACT_APP_DISCORD_TOKEN)
+
+const app = express()
+app.use(express.static(path.join(__dirname, '/build')))
+
+app.get('/api/getList', (req, res) => {
+  var list = ['item1', 'item2', 'item3']
+  res.json(list)
+  console.log('Sent list of items')
+})
+
+app.get('/api/test', (req, res) => {
+  var list = ['item1', 'item2', 'item3']
+  const guild = client.guilds.cache.get('317652808641806350')
+  const { settings } = guild
+  if (settings._commandPrefix !== '!') {
+    guild.commandPrefix = '!'
+  }
+  res.json(list)
+})
+
+app.get('/api/getServer', [body('token').not().isEmpty()], async (req, res) => {
+  const errors = validationResult(req)
+  console.log('errors', errors)
+  res.type('json')
+  var list = ['item1', 'item2', 'item3']
+  res.json(list)
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'))
+})
+
+const port = process.env.PORT || 5000
+app.listen(port)
+
+console.log('App is listening on port ' + port)
+
+// api with discord
